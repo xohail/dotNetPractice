7e60110bb8caa1eb6eccf3543bc94896d622e10b
Sohail Rahman <engr.sohail36@gmail.com>
2025-11-21 00:04:20 +1100

    Fabonnaci with recursion

--- Program.cs
+++ Program.cs
@@ -6,15 +6,17 @@
         System.Console.WriteLine("\n");
 
         FabonnaciSeries();
+        System.Console.WriteLine("\n");
+        FabonnaciSeriesWithRecursion();
     }
 
     public static void FabonnaciSeries()
     {
         System.Console.WriteLine("\u001b[1mFabonnaci series \u001b[0m");
 
-        var a = 0; 
-        var b = 1;
-        var sum = 0;
+        int a = 0; 
+        int b = 1;
+        int sum = 0;
         System.Console.WriteLine(a);
 
         while(a<100)
@@ -25,6 +27,24 @@
 
             System.Console.WriteLine(a);
         }
+    }
+
+    public static void FabonnaciSeriesWithRecursion()
+    {
+        System.Console.WriteLine("\u001b[1mFabonnaci series with recursion\u001b[0m");
+
+        int n = 10; // Number of terms to display
+        for (int i = 0; i < n; i++)
+        {
+            System.Console.WriteLine(Fib(i));
+        }
+    }
+
+    private static int Fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return Fib(n - 1) + Fib(n - 2);
     }
 }
 
